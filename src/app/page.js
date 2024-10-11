eccbbe07a71022613979f7ef7c20962232f16073
dhruvv-raghu <raghudhruv2@gmail.com>
2024-10-11 17:55:08 +0530

    checking with login

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
     return (
         <main className="bg-base-200 min-h-screen flex flex-col pt-20"> {/* Added pt-20 */}
             <div className="flex flex-col items-center justify-center flex-1 text-center">
-                <h1 className="text-6xl font-bold text-primary-content mb-4">Welcome to Your App</h1>
-                <p className="text-lg text-primary-content mb-8">Discover the power of personalized financial insights.</p>
+                <h1 className="text-6xl text-cyan-500 font-bold text-primary-content mb-4">Welcome to Your App</h1>
+                <p className="text-cyan-500 text-primary-content mb-8">Discover the power of personalized financial insights.</p>
                 <Link href="/login">
                     <button className="btn btn-primary btn-lg">Get Started</button>
                 </Link>
