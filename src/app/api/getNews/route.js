21cf978117cc2b7bd8049548438e70b55f499a3d
dhruvv-raghu <raghudhruv2@gmail.com>
2024-11-04 11:45:44 +0530

    holdings there

--- src/app/api/getNews/route.js
+++ src/app/api/getNews/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const apiKey = process.env.NEWS_API_KEY; // Store your API key in an environment variable
-  const url = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=technology,ipo,economy,markets,mergers_and_acquisitions&limit=100&apikey=${apiKey}`;
+  const url = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=technology,ipo,economy,markets,mergers_and_acquisitions&limit=2&apikey=${apiKey}`;
 
   try {
     const response = await fetch(url, {
--- src/app/holdings/layout.js
+++ src/app/holdings/layout.js
@@ -1,19 +1,14 @@
-import Link from "next/link"
+import '../globals.css';
+import { Toaster } from 'react-hot-toast'; // Import Toaster
+import PageNavbar from '../components/PageNavbar';
 
-export default function PageNavbar() {
-  const NavLink = ({ href, children }) => (<Link href={href} className="text-white font-bold hover:text-[#4ac1ff] transition-colors duration-200"> {children} </Link>)
-
-  return (
-       <nav className="bg-gradient-to-r from-[#001f3f] to-black p-6"> 
-           <div className="flex justify-between items-center"> 
-               <Link href="/" className="text-[#4ac1ff] font-bold text-xl"> StockSavvy </Link> 
-                   <div className="flex items-center space-x-4"> 
-                       <NavLink href="/watchlist">Watchlist</NavLink> 
-                       <NavLink href="/orders">Orders</NavLink> 
-                       <NavLink href="/holdings">Holdings</NavLink> 
-                       <NavLink href="/dashboard">Dashboard</NavLink> 
-                       <NavLink href="/admin">Admin</NavLink> 
-                    </div> 
+export default function Layout({ children }) {
+    return (
+            <div className="bg-gradient-to-r from-blue-500 to-cyan-500">
+            <PageNavbar />
+            {children}
             </div> 
-      </nav>)
-};
+    );
+}
+
+
